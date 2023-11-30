03f37c80e74849ff0b45c089194f9ed304c05379
ADMIN\Admin <truongcongduy.it@gmail.com>
2023-11-30 21:21:02 +0700

    add HardcodeDataCustomerView.cs file

---
+++ Activities/HardCodeDataCustomerView.cs
@@ -0,0 +1,23 @@
+﻿using CloudBanking.BaseControl;
+using CloudBanking.Common;
+using CloudBanking.Entities;
+using CloudBanking.Flow.Base;
+using CloudBanking.Language;
+using CloudBanking.ShellContainers;
+using CloudBanking.ShellModels;
+using CloudBanking.ShellUI;
+using CloudBanking.UI;
+using CloudBanking.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static CloudBanking.Utilities.UtilEnum;
+using static Java.Util.Jar.Attributes;
+
+namespace CloudBanking.UITestApp
+{
+    public partial class TestActivity : BaseActivity
+    {
+
+    }
+}
